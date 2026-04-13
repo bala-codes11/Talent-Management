23ad474936c625c93d67aa73461efd8537ba4494
Balan <balan.xyloinc2025@gmail.com>
2026-04-13 13:41:12 +0530

    sprint 4

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // 🔥 ENV VALIDATION
 if (!process.env.JWT_SECRET) {
-  throw new Error("JWT_SECRET is missing in .env");
+  throw new Error("JWT_SECRET is missing in .env"); 
 }
 if (!process.env.MONGO_URI) {
   throw new Error("MONGO_URI is missing in .env");
